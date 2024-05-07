b7cf16b83bceade26160e5a01dbd8621c1afd011
Janine Khuc <janine.m.khuc@gmail.com>
2024-05-07 10:05:18 +0200

    add dbx templates

---
+++ templates/dbx_run_all.py
@@ -0,0 +1,120 @@
+# Databricks notebook source
+# MAGIC %run "./SQL/create_silver_MV_afis_bedrijf.sql"
+
+# COMMAND ----------
+
+# MAGIC %run "./SQL/create_silver_MV_afis_boekingscombinatie.sql"
+
+# COMMAND ----------
+
+# MAGIC %run "./SQL/create_silver_MV_afis_budget.sql"
+
+# COMMAND ----------
+
+# MAGIC %run "./SQL/create_silver_MV_afis_budgetplaats.sql"
+
+# COMMAND ----------
+
+# MAGIC %run "./SQL/create_silver_MV_afis_budgetplaats_hierarchie.sql"
+
+# COMMAND ----------
+
+# MAGIC %run "./SQL/create_silver_MV_afis_budgetplaats_hier_knoop.sql"
+
+# COMMAND ----------
+
+# MAGIC %run "./SQL/create_silver_MV_afis_budgetpositie.sql"
+
+# COMMAND ----------
+
+# MAGIC %run "./SQL/create_silver_MV_afis_budgetpositie_hierarchie.sql"
+
+# COMMAND ----------
+
+# MAGIC %run "./SQL/create_silver_MV_afis_budgetpositie_hier_knoop.sql"
+
+# COMMAND ----------
+
+# MAGIC %run "./SQL/create_silver_MV_afis_controlgebied.sql"
+
+# COMMAND ----------
+
+# MAGIC %run "./SQL/create_silver_MV_afis_fonds.sql"
+
+# COMMAND ----------
+
+# MAGIC %run "./SQL/create_silver_MV_afis_fonds_hierarchie.sql"
+
+# COMMAND ----------
+
+# MAGIC %run "./SQL/create_silver_MV_afis_fonds_hier_knoop.sql"
+
+# COMMAND ----------
+
+# MAGIC %run "./SQL/create_silver_MV_afis_kostenplaats.sql"
+
+# COMMAND ----------
+
+# MAGIC %run "./SQL/create_silver_MV_afis_kostenplaats_hierarchie.sql"
+
+# COMMAND ----------
+
+# MAGIC %run "./SQL/create_silver_MV_afis_kostensoort.sql"
+
+# COMMAND ----------
+
+# MAGIC %run "./SQL/create_silver_MV_afis_kostensoort_hierarchie.sql"
+
+# COMMAND ----------
+
+# MAGIC %run "./SQL/create_silver_MV_afis_land.sql"
+
+# COMMAND ----------
+
+# MAGIC %run "./SQL/create_silver_MV_afis_profitcenter.sql"
+
+# COMMAND ----------
+
+# MAGIC %run "./SQL/create_silver_MV_afis_programma.sql"
+
+# COMMAND ----------
+
+# MAGIC %run "./SQL/create_silver_MV_afis_programma_hierarchie.sql"
+
+# COMMAND ----------
+
+# MAGIC %run "./SQL/create_silver_MV_afis_programma_hier_knoop.sql"
+
+# COMMAND ----------
+
+# MAGIC %run "./SQL/create_silver_MV_afis_project.sql"
+
+# COMMAND ----------
+
+# MAGIC %run "./SQL/create_silver_MV_afis_realisatie.sql"
+
+# COMMAND ----------
+
+# MAGIC %run "./SQL/create_silver_MV_afis_rekeningschema.sql"
+
+# COMMAND ----------
+
+# MAGIC %run "./SQL/create_silver_MV_afis_taakveld_budget.sql"
+
+# COMMAND ----------
+
+# MAGIC %run "./SQL/create_silver_MV_afis_taakveld_realisatie.sql"
+
+# COMMAND ----------
+
+# MAGIC %run "./SQL/create_silver_MV_afis_uitnutting.sql"
+
+# COMMAND ----------
+
+# MAGIC %run "./SQL/create_silver_MV_afis_wbs_element.sql"
+
+# COMMAND ----------
+
+# MAGIC %run "./SQL/create_silver_MV_afis_wbs_element_hierarchie.sql"
+
+# COMMAND ----------
